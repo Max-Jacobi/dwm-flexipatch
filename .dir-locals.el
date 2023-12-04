e54aa419e7bd4c68bcde2022acd8fbb9a550036b
Max Jacobi <mjacobi@theorie.ikp.physik.tu-darmstadt.de>
2023-12-04 11:57:08 +0100

    make clean in dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,1 @@
-((nil . ((projectile-project-install-cmd . "sudo -A make install && kill -HUP $(pgrep -u $USER 'dwm$')"))))
+((nil . ((projectile-project-install-cmd . "sudo -A make clean install && kill -HUP $(pgrep -u $USER 'dwm$')"))))
